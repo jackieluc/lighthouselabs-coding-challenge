201dffaa11a9f017fe0829e44cde9e2c54fe8314
Jackie Luc <jackieluc17@gmail.com>
2018-10-06 19:34:07 -0600

    challenge 6

--- index.js
+++ index.js
@@ -102,3 +102,21 @@ const convertColumn = (coordinate) => {
 
     return columnIndex;
 };
+
+/**
+ * Challenge #6
+ * 
+ * Okay, let's get our lighthouse actually doing some work! As the lighthouse keeper, 
+ * sometimes you'll need to shine the light directly on a particular cell in the grid,
+ * to alert passing ships to a danger there.
+ * 
+ * Your job is to write a function called lightCell() that takes in the coordinates in the form 
+ * of 'A3' or 'J9' and returns the contents of that specific cell. (Ex: lightCell('B4'); would return "")
+ */
+
+const lightCell = (coordinate) => {
+    const rowIndex = coordinate.charAt(1) - 1;
+    const columnIndex = convertColumn(coordinate);
+    
+    return GRID[rowIndex][columnIndex];
+  };
