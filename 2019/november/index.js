c0b7edbfc7866c60cc090580df0acabd0cd0cdd0
Jackie Luc <jackieluc17@gmail.com>
2019-11-02 14:05:06 -0600

    Challenge #2

--- 2019/november/index.js
+++ 2019/november/index.js
@@ -10,3 +10,19 @@ const doorToDoor = (volunteers, neighbourhoods) => {
   
   return Math.floor(numberOfNeighbourhoods / numberOfVolunteers);
 };
+
+// Challenge #2
+const interviewAnswer = (topic) => {
+  
+  switch(topic) {
+
+    case 'arts funding':
+      return 'We\'ll have to get creative!';
+    case 'economy':
+      return 'Time is money.';
+    case 'transportation':
+      return 'It\'s going to be a long road, so we better get moving.';
+    default:
+      return 'QUACK!';
+  }
+};
